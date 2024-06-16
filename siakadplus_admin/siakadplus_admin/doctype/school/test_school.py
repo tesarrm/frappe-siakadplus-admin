5d08f5a605839585fd5e473b60b34b5539e24037
Tesar Rahmat Maulana <tesarrm58@gmail.com>
2024-06-16 10:27:29 +0700

    feat: add school docytpe

---
+++ siakadplus_admin/siakadplus_admin/doctype/school/test_school.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Tesar Rahmat Maulana and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestSchool(FrappeTestCase):
+	pass
